0ecaeaee4534c036e7e5f63be0081057183d3c08
TheMrX234 <71405616+TheMrX234@users.noreply.github.com>
2020-09-16 14:03:48 -0500

    Add files via upload

---
+++ eclipse-workspace/BetaDev/src/main/java/com/carloscueto/alumnos/BetaDevApplication.java
@@ -0,0 +1,13 @@
+package com.carloscueto.alumnos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BetaDevApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BetaDevApplication.class, args);
+	}
+
+}
---
+++ eclipse-workspace/BetaDev/src/test/java/com/carloscueto/alumnos/BetaDevApplicationTests.java
@@ -0,0 +1,13 @@
+package com.carloscueto.alumnos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BetaDevApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
